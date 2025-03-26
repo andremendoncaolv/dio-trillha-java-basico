de2bd01ce542caea3cf373a189321638ff2b9008
andremendoncaolv <andremendoncaolv@gmail.com>
2025-03-26 11:26:07 -0300

    desafio conta

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner =  new Scanner(System.in).useLocale(Locale.US);        
+        
+        System.out.println("Por favor, digite o nome!");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite a Agência !");
+        String agencia = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o número da Agência !");
+        int numero = scanner.nextInt();
+
+
+        System.out.println("Por favor, digite o valor a depositar!");
+        double saldo = scanner.nextDouble();
+
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
     }
     
+
+
+
 }
